64b5d9326ee27fc5fdc5d5f8e9a07c7483ebec6b
huangxin <1377186691@qq.com>
2019-09-29 10:59:41 +0800

    增加@Override注解

--- src/main/java/com/hx/service/impl/TestService.java
+++ src/main/java/com/hx/service/impl/TestService.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TestService implements ITestService {
+
+    @Override
     public void add(String name) {
         System.out.println("新增了" + name);
     }
 
+    @Override
     public void query(String name, String age) {
         System.out.println(name + "的年龄是" + age);
     }
